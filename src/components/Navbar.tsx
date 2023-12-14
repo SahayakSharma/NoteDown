aad42eea375dd3b6e93b540e23a0fe5c9e0f5edf
Mehra-Jatin <jatinmehra578@gmail.com>
2023-12-14 20:59:34 +0530

    navbar modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,11 @@ import { useRouter } from 'next/navigation'
 const Navbar = () => {
   const router=useRouter();
   return (
-    <div className='w-[100vw] h-[80px] text-black items-center bg-white flex justify-around'>
-        <h2>HOME</h2>
-        <h2 onClick={()=>{router.push('/SignIn')}}>SignIn</h2>
-        <h2 onClick={()=>{router.push('/')}}>LogIn</h2>
-        <h2>Diary</h2>
+    <div className='w-[100vw] h-[80px] text-black items-center bg-[#FFA500] flex justify-around rounded-[20px]'>
+        <button className='w-[50px] h-[30px]  text-black hover:text-white bg-transparent hover:bg-[#FFA600]  '>HOME</button>
+        <button className='w-[50px] h-[30px]  text-black hover:text-white bg-transparent hover:bg-[#FFA600]  ' onClick={()=>{router.push('/SignIn')} }>SignIn</button>
+        <button className='w-[50px] h-[30px]  text-black hover:text-white bg-transparent hover:bg-[#FFA600]  'onClick={()=>{router.push('/')}}>LogIn</button>
+        <button className='w-[50px] h-[30px]  text-black hover:text-white bg-transparent hover:bg-[#FFA600]  '>Diary</button>
 
       
     </div>
